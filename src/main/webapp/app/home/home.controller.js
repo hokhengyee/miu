d81d9a79c22d6cd2ac102b4c86098bf5b6d4a798
HO KHENG YEE <ho.kheng.yee@gmail.com>
2017-02-25 02:00:08 +0800

    Added Carousel in homepage.

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -38,5 +38,40 @@
 		function register() {
 			$state.go('register');
 		}
+
+		/* Carousel */
+		$scope.myInterval = 5000;
+		$scope.noWrapSlides = false;
+		$scope.active = 0;
+		var slides = $scope.slides = [];
+		var currIndex = 0;
+
+		$scope.addSlide = function() {
+			var newWidth = 600 + slides.length + 1;
+			slides.push({
+				image : '/content/images/banner/banner' + currIndex + '.jpg',
+				// text : [ 'Nice image', 'Awesome photograph', 'That is so
+				// cool',
+				// 'I love that' ][slides.length % 4],
+				id : currIndex++
+			});
+		};
+
+		$scope.randomize = function() {
+			var indexes = generateIndexesArray();
+			assignNewIndexesToSlides(indexes);
+		};
+
+		for (var i = 0; i < 4; i++) {
+			$scope.addSlide();
+		}
+
+		// Randomize logic below
+
+		function assignNewIndexesToSlides(indexes) {
+			for (var i = 0, l = slides.length; i < l; i++) {
+				slides[i].id = indexes.pop();
+			}
+		}
 	}
 })();
